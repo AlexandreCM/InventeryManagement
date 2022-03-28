adb7599f326ef462113054367f98572084693508
AlexandreCM <alexandre.mezeray@gmail.com>
2022-03-28 09:24:51 -0400

    add base project

---
+++ src/test/java/com/alexandrecm/inventoryManagement/InventoryManagementApplicationTest.java
@@ -0,0 +1,11 @@
+package com.alexandrecm.inventoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTest {
+
+  @Test
+  void contextLoads() {}
+}
